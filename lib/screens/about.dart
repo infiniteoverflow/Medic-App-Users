14c6b83a431b00b6392af8d22ae68c4a2cdc5a32
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-09-14 21:17:40 +0530

    About Page UI

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -16,14 +16,46 @@ class AboutState extends State<About> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "About Us"
+          "About Us",
+          style: TextStyle(
+            color: Colors.black
+          ),
+        ),
+        backgroundColor: Colors.amber,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,color: Colors.black,),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
-      body: Center(
-        child: Text(
-          "Hello World"
-        ),
-      ),
+      body: ListView(
+        children: <Widget>[
+
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+          ),
+
+          Text(
+            "Ashirwad Nursing Home",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: "Patrick",
+              fontSize: 35
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top: 10,left: 25,right: 25),
+            child: Card(
+              child: Image(
+                image: AssetImage("assets/images/hospital/hos2.jpg"),
+              ),
+            ),
+          )
+
+        ],
+      )
     );
   }
 

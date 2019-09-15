18666310940ca2927cb0904ab8b1c6bc8748b593
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-09-15 10:12:29 +0530

    Set the About Us UI

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -108,6 +108,257 @@ class AboutState extends State<About> {
             ),
           ),
 
+          Padding(
+            padding: EdgeInsets.only(top: 10,left: 10),
+            child: Text(
+              "Main Specialities :",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                  fontFamily: "Patrick",
+                  fontSize: 35
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "1. General and Laparoscopic Surgery",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top:20,left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Lap Appendex",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Lap Cholecystectomy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Lap hysterectomy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Lap ovarian cystectomy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Lap Surgeries and diagnostic laproscopy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "2. Obsterics and Gynecology",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top:20,left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Normal Deliveries",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "LSCS",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Diagnostic Lap",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Hysterectomy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Myomectomy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Counselling",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "3. Orthopedics",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top:20,left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "All types of Fractures and bone diseases are dealt with "
+                    "latest technologies",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "4. Urology",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top:20,left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "All type of stone management and prostate,tumors and infertility etc"
+                    "\n\n Like - Turp , turbt cystoscopic removal of renal , "
+                    "ureteric , bladder and urethral stones",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top:20,left: 10),
+            child: ListTile(
+              leading: Icon(Icons.fiber_manual_record,color: Colors.black,),
+              title: Text(
+                "Corrective Surgeries for Congential urological problems",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
 
         ],
       )
